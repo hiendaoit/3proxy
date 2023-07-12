cf88381e4266a774cbe3a3bd3629070e3cd28061
hiendaoit <72371579+hiendaoit@users.noreply.github.com>
2023-07-12 16:40:47 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -58,6 +58,16 @@ $(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
 }
 
+upload_proxy() {
+    local PASS=$(random)
+    zip --password $PASS proxy.zip proxy.txt
+    URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
+
+    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
+    echo "Download zip archive from: ${URL}"
+    echo "Password: ${PASS}"
+
+}
 
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
@@ -111,6 +121,7 @@ EOF
 bash /etc/rc.local
 
 gen_proxy_file_for_user
+upload_proxy
 rm -rf /root/setup.sh
 rm -rf /root/3proxy-3proxy-0.8.6
 
